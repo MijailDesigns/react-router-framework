c7cf7f4f7f26f33224f65b39dd4e0043594fa745
Mijail Pulgar <mijail86@gmail.com>
2025-05-19 14:06:49 -0500

    diseño de auth layout, login y register

--- app/layouts/auth-layout.tsx
+++ app/layouts/auth-layout.tsx
@@ -1,13 +1,11 @@
-import React from "react";
 import { Outlet } from "react-router";
 
-const AutLayout = () => {
+export default function LoginPage() {
   return (
-    <div>
-      <h1>AuthLayout</h1>
-      <Outlet />
+    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">
+        <Outlet />
+      </div>
     </div>
   );
-};
-
-export default AutLayout;
+}
