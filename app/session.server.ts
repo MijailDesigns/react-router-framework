15b0207aa810f3e81dcaa948bd4f309a9116a188
Mijail Pulgar <mijail86@gmail.com>
2025-05-20 12:19:34 -0500

    update domain url

--- app/session.server.ts
+++ app/session.server.ts
@@ -17,7 +17,7 @@ const { getSession, commitSession, destroySession } =
       name: "__session",
 
       // all of these are optional
-      domain: "localhost",
+      domain: "https://react-router-framework-f7fk.onrender.com/",
       // Expires can also be set (although maxAge overrides it when used in combination).
       // Note that this method is NOT recommended as `new Date` creates only one date on each server deployment, not a dynamic date in the future!
       //
